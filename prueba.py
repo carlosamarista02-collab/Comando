cd0669d89f18d83ee860e470b0a351547998acce
carlosamarista02-collab <carlosamarista02@gmail.com>
2026-06-24 11:49:02 -0500

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -14,7 +14,7 @@ from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 # CONFIGURACIÓN INICIAL
 # ==========================================
 TOKEN_TELEGRAM = '8939217389:AAHDVYsmfx8TFCbjtrZHlIfppajsPluJcQA'
-URL_MINI_APP = 'https://glistening-buttercream-e53584.netlify.app/' 
+URL_MINI_APP = 'https://reliable-heliotrope-796c0a.netlify.app/' 
 ADMIN_ID = 6808824866 
 
 # URL definitiva usando el Transaction Pooler (IPv4 compatible con Render)
