7512d14598f779d7f4a683e949c5d707a6be3bdf
carlosamarista02-collab <carlosamarista02@gmail.com>
2026-06-29 15:44:37 -0500

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -16,9 +16,9 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 # --- Configuración de Telegram ---
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "8939217389:AAHDVYsmfx8TFCbjtrZHlIfppajsPluJcQA")
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "8939217389:AAFBjWVad0weeypJMh8nP9Rm8f5cXMIo35M")
 ADMIN_ID = int(os.getenv("ADMIN_TELEGRAM_ID", "6808824866"))
-GRUPO_TELEGRAM_ID = int(os.getenv("GRUPO_TELEGRAM_ID", "-1001234567890"))  # ID del grupo
+GRUPO_TELEGRAM_ID = int(os.getenv("GRUPO_TELEGRAM_ID", "-0"))  # ID del grupo
 
 try:
     bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
