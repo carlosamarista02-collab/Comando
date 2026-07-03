ea9bcca51f6d8151cd35e511bc98eb8c620e5806
carlosamarista02-collab <carlosamarista02@gmail.com>
2026-07-03 18:36:03 -0500

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -7,7 +7,7 @@ import threading
 import time
 
 # ============ CONFIGURACIÓN ============
-BOT_TOKEN = "8206009148:AAEEWSYAgxj3MRR8xGOe-s7V5COl5htsYnY"
+BOT_TOKEN = "8206009148:AAGPVgO2VLfKYcUNy-BlBGWfv40gwrFivHQ"
 ADMIN_ID = 6808824866
 API_URL = "http://localhost:8000"
 
